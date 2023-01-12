022b898cee8b6d3e133b28fb7f3aaa83e1161f79
TestUser <testuser@example.com>
2023-01-12 21:35:25 +0530

    new file

---
+++ src/main/java/com/sample/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.sample.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	
+	@GetMapping("/welcome")
+	public string welcome(){
+		return "welcome to javatech"
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
